b7b30581667a9ce79ffb764a0b4c4e5541e38e01
Alex Zhou <alex.zzh.s7@gmail.com>
2021-12-27 02:51:17 +0900

    Limit newsletter addresses

--- newsletter/index.js
+++ newsletter/index.js
@@ -88,13 +88,15 @@ async function main() {
       },
     })
 
-    const addresses = (await prisma.user.findMany({
-      select: {
-        email: true
-      }
-    }))
-      .map(i => i.email)
-      .join(',')
+    // const addresses = (await prisma.user.findMany({
+    //   select: {
+    //     email: true
+    //   }
+    // }))
+    //   .map(i => i.email)
+    //   .join(',')
+    
+    const addresses = ['ryofujimoto@icloud.com', 'mainonozawa@gmail.com', 'sandrasoso@hotmail.fr', 'alex.zzh.s7@gmail.com'].join(',')
     
     await transporter.sendMail({
       from: '"Patoken" <noreply@mail.patoken.org>',
